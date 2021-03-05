d16e71e439fc6667e450400d4b34204b2050a630
xliel <48176913+xliel@users.noreply.github.com>
2021-03-05 06:04:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,42 +3,42 @@ const { readdirSync } = require("fs");
 const config = require("./config");
 const { prefix, token } = config;
 const client = new Client({
-  disableMentions: 'everyone',
-  fetchAllMembers: true
+    disableMentions: 'everyone',
+    fetchAllMembers: true
 });
 
 client.login(token);
 
 // Updates the bot's status when he online
 client.on("ready", async () => {
-  console.log(`Bot is Online!`);
-  // typs: ['PLAYING', 'WATCHING', 'LISTENING', 'STREAMING']
-  client.user.setActivity(`${prefix}help | Made by xliel#6666`, { type: "WATCHING" });
+    console.log(`Bot is Online!`);
+    // typs: ['PLAYING', 'WATCHING', 'LISTENING', 'STREAMING']
+    client.user.setActivity(`${prefix}help | Made by xliel#6666`, { type: "WATCHING" });
 });
 
 client.on("message", async message => {
-  if (message.author.bot) return;
-  if (message.channel.type === "dm") return;
+    if (message.author.bot) return;
+    if (message.channel.type === "dm") return;
 
-  let msg = message.content.toLowerCase() || message.content.toUpperCase();
-  if (!msg.startsWith(prefix)) return;
-  let args = message.content.slice(prefix.length).trim().split(" ");
-  const command = args.shift().toLowerCase();
+    let msg = message.content.toLowerCase() || message.content.toUpperCase();
+    if (!msg.startsWith(prefix)) return;
+    let args = message.content.slice(prefix.length).trim().split(" ");
+    const command = args.shift().toLowerCase();
 
-  if (command === "ping") {
-message.channel.send("Pong!"); // Normal Message
-  }
-  if (command === "hey") {
-message.reply("Hello!"); // Reply Message
-  }
-  if (command === "help") {
-message.author.send("your help commands"); // DM Message
-  }
-  if (command === "embed") {
-let embed = new MessageEmbed()
-.setTitle("Title")
-.setDescription("Description");
+    if (command === "ping") {
+        message.channel.send("Pong!"); // Normal Message
+    }
+    if (command === "hey") {
+        message.reply("Hello!"); // Reply Message
+    }
+    if (command === "help") {
+        message.author.send("your help commands"); // DM Message
+    }
+    if (command === "embed") {
+        let embed = new MessageEmbed()
+            .setTitle("Title")
+            .setDescription("Description");
 
-message.author.send(embed); // Embed Message
-  }
+        message.author.send(embed); // Embed Message
+    }
 });
